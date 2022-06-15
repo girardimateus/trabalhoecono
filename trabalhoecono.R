349bc1c78f4f7c7417b3cc5d0ca14f551f381bc7
girardimateus <88946090+girardimateus@users.noreply.github.com>
2022-06-15 19:36:09 -0300

    Add files via upload

--- trabalhoecono.R
+++ trabalhoecono.R
@@ -26,30 +26,35 @@ base$rendamedia <- ifelse(base$rendabaixa == 0 & base$rendaalta == 0,1,0)
 #Modelo simples
 regsimples <- lm(inflacao ~ cbi_paises, base)
 summary(regsimples)
+coeftest(regsimples, vcov = vcovHC(regsimples,"HC0"))
 ersimples <- vcovHC(regsimples, type = "HC")
 robust_simples <- sqrt(diag(ersimples))
 
 #Modelo controlando por gasto
 reg1 <- lm(inflacao ~ cbi_paises + gasto, base)
 summary(reg1)
+coeftest(reg1, vcov = vcovHC(reg1,"HC0"))
 er1 <- vcovHC(reg1, type = "HC")
 robust_1 <- sqrt(diag(er1))
 
 #Modelo controlando por gasto e variação do pib
 reg2 <- lm(inflacao ~ cbi_paises + gasto + pibvar, base)
 summary(reg2)
+coeftest(reg2, vcov = vcovHC(reg2,"HC0"))
 er2 <- vcovHC(reg2, type = "HC")
 robust_2 <- sqrt(diag(er2))
 
 #Modelo controlando por gasto, variação do pib e nivel de renda do país
 reg3 <- lm(inflacao ~ cbi_paises + gasto + pibvar + rendabaixa + rendaalta + rendamedia, base)
 summary(reg3)
+coeftest(reg3, vcov = vcovHC(reg3,"HC0"))
 er3 <- vcovHC(reg3, type = "HC")
 robust_3 <- sqrt(diag(er3))
 
 #Modelo controlando por gasto, variação do pib, nivel de renda do país e nivel de political constrain
 reg4 <- lm(inflacao ~ cbi_paises + gasto + pibvar + rendabaixa + rendaalta + rendamedia + pclow + pchigh + pcmed, base)
 summary(reg4)
+coeftest(reg4, vcov = vcovHC(reg4,"HC0"))
 er4 <- vcovHC(reg4, type = "HC")
 robust_4 <- sqrt(diag(er4))
 
@@ -59,27 +64,26 @@ stargazer(regsimples, reg1, reg2, reg3, reg4, se=list(robust_simples,robust_1,ro
           align=T,
           type = "text")
 
-
 #Estatisticas descritivas
-stargazer(base, type = "text", summary.stat = c("n","mean","median","sd","max","min"))
+stargazer(as.data.frame(base), type = "text", summary.stat = c("n","mean","median","sd","max","min"))
 
 #Estatísticas descritivas por renda
 base_rb <- filter(base, rendabaixa == 1)
 base_rm <- filter(base, rendamedia == 1)
 base_ra <- filter(base, rendaalta == 1)
 
-stargazer(base_rb, type = "text")
-stargazer(base_rm, type = "text")
-stargazer(base_ra, type = "text")
+stargazer(as.data.frame(base_rb), type = "text")
+stargazer(as.data.frame(base_rm), type = "text")
+stargazer(as.data.frame(base_ra), type = "text")
 
 #Estatísticas descritivas por restrição
 base_pcb <- filter(base, pclow == 1)
 base_pcm <- filter(base, pcmed == 1)
 base_pca <- filter(base, pchigh == 1)
 
-stargazer(base_pcb, type = "text")
-stargazer(base_pcm, type = "text")
-stargazer(base_pca, type = "text")
+stargazer(as.data.frame(base_pcb), type = "text")
+stargazer(as.data.frame(base_pcm), type = "text")
+stargazer(as.data.frame(base_pca), type = "text")
 
 
 #Regressão simples - Gráfico
@@ -88,13 +92,5 @@ regsimples_grafico <- ggplot(data = base) + geom_point(aes(x=cbi_paises, y=infla
   labs(x = 'Independência do Banco Central',y = 'Inflação') +  theme_few() 
 regsimples_grafico
 
-#Gráfico CBI
-cbi_grafico <- ggplot(data = base) + geom_density(aes(x=cbi_paises), color = "#1F1B1B", size = 1, shape = 20) + ylim(0,2.5) +
-  labs(x = 'Independência do Banco Central',y = 'Inflação') +  theme_few() 
-cbi_grafico
 
-
-#Apendice
-base_arredondada <- base %>% mutate(across(where(is.numeric), round, 3))
-stargazer(base_arredondada[1:100,], summary=FALSE, rownames=FALSE, type = "latex", digits = 2)
 
